205f55a9a19f1c8f6413dba8c3c36c52298e11ca
Satyam Prajapati <satyam7355pra@gmail.com>
2021-12-31 17:11:14 +0530

    fifth commit setup complete

--- app/src/main/java/com/satyam/cricketstatus/MemberDetails.java
+++ app/src/main/java/com/satyam/cricketstatus/MemberDetails.java
@@ -7,8 +7,13 @@ import android.os.Bundle;
 import android.view.View;
 
 public class MemberDetails extends AppCompatActivity {
-Intent intent;
+    Intent intent;
+    static String txtName, txtRole, txtMatchTestBat, txtMatchOdiBat, txtMatchT20Bat,
+            txtMatchTestBowl, txtMatchOdiBowl, txtMatchT20Bowl;
+    static String txtRunsTest,txt50sTest,txt100sTest,txtRunsOdi,txt50sOdi,txt100sOdi,txtRunsT20,txt50sT20,txt100sT20;
+    static String txtWicketsTest,txtEcoTest,txtBestTest,txtWicketsOdi,txtEcoOdi,txtBestOdi,txtWicketsT20,txtEcoT20,txtBestT20;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_member_details);
@@ -16,122 +21,193 @@ Intent intent;
 
     public void onVirat(View view) {
         intent = new Intent(MemberDetails.this, Statistics.class);
-        intent.putExtra("image",R.drawable.viratkohli);
+        intent.putExtra("image", R.drawable.viratkohli);
         startActivity(intent);
     }
 
     public void onShikhar(View view) {
+        commonDetails("Shikhar Dhawan","Batsman","34","145","68","34","145","68");
+        battingDetails("2315","5","7","6105","33","17","1759","11","0");
+        bowlingDetails("0","2.0","0/0","0","0","0/0","0","0","0/0");
         intent = new Intent(MemberDetails.this, Statistics.class);
-        intent.putExtra("image",R.drawable.shikhardhawan2);
+        intent.putExtra("image", R.drawable.shikhardhawan2);
+
+        intent.putExtra("txtName",txtName);
+        intent.putExtra("txtRole",txtRole);
+        intent.putExtra("txtMatchTestBat",txtMatchTestBat);
+        intent.putExtra("txtMatchOdiBat",txtMatchOdiBat);
+        intent.putExtra("txtMatchT20Bat",txtMatchT20Bat);
+        intent.putExtra("txtMatchTestBowl",txtMatchTestBowl);
+        intent.putExtra("txtMatchOdiBowl",txtMatchOdiBowl);
+        intent.putExtra("txtMatchT20Bowl",txtMatchT20Bowl);
+
+        intent.putExtra("txtRunsTest",txtRunsTest);
+        intent.putExtra("txt50sTest",txt50sTest);
+        intent.putExtra("txt100sTest",txt100sTest);
+        intent.putExtra("txtRunsOdi",txtRunsOdi);
+        intent.putExtra("txt50sOdi",txt50sOdi);
+        intent.putExtra("txt100sOdi",txt100sOdi);
+        intent.putExtra("txtRunsT20",txtRunsT20);
+        intent.putExtra("txt50sT20",txt50sT20);
+        intent.putExtra("txt100sT20",txt100sT20);
+
+        intent.putExtra("txtWicketsTest",txtWicketsTest);
+        intent.putExtra("txtEcoTest",txtEcoTest);
+        intent.putExtra("txtBestTest",txtBestTest);
+        intent.putExtra("txtWicketsOdi",txtWicketsOdi);
+        intent.putExtra("txtEcoOdi",txtEcoOdi);
+        intent.putExtra("txtBestOdi",txtBestOdi);
+        intent.putExtra("txtWicketsT20",txtWicketsT20);
+        intent.putExtra("txtEcoT20",txtEcoT20);
+        intent.putExtra("txtBestT20",txtBestT20);
         startActivity(intent);
     }
 
     public void onRohit(View view) {
         intent = new Intent(MemberDetails.this, Statistics.class);
-        intent.putExtra("image",R.drawable.rohitsharma3);
+        intent.putExtra("image", R.drawable.rohitsharma3);
         startActivity(intent);
     }
 
     public void onShreyas(View view) {
         intent = new Intent(MemberDetails.this, Statistics.class);
-        intent.putExtra("image",R.drawable.shreyasiyer4);
+        intent.putExtra("image", R.drawable.shreyasiyer4);
         startActivity(intent);
     }
 
     public void onPant(View view) {
         intent = new Intent(MemberDetails.this, Statistics.class);
-        intent.putExtra("image",R.drawable.rishabhpant5);
+        intent.putExtra("image", R.drawable.rishabhpant5);
         startActivity(intent);
     }
 
     public void onJadeja(View view) {
         intent = new Intent(MemberDetails.this, Statistics.class);
-        intent.putExtra("image",R.drawable.ravindrajadeja6);
+        intent.putExtra("image", R.drawable.ravindrajadeja6);
         startActivity(intent);
     }
 
     public void onHardik(View view) {
         intent = new Intent(MemberDetails.this, Statistics.class);
-        intent.putExtra("image",R.drawable.hardikpandya7);
+        intent.putExtra("image", R.drawable.hardikpandya7);
         startActivity(intent);
     }
 
     public void onShami(View view) {
         intent = new Intent(MemberDetails.this, Statistics.class);
-        intent.putExtra("image",R.drawable.mohammadshami8);
+        intent.putExtra("image", R.drawable.mohammadshami8);
         startActivity(intent);
     }
 
     public void onChahal(View view) {
         intent = new Intent(MemberDetails.this, Statistics.class);
-        intent.putExtra("image",R.drawable.yuzvendrachahal9);
+        intent.putExtra("image", R.drawable.yuzvendrachahal9);
         startActivity(intent);
     }
 
     public void onBhuvneshwar(View view) {
         intent = new Intent(MemberDetails.this, Statistics.class);
-        intent.putExtra("image",R.drawable.bhuvneshwarkumar10);
+        intent.putExtra("image", R.drawable.bhuvneshwarkumar10);
         startActivity(intent);
     }
 
     public void onDeepak(View view) {
         intent = new Intent(MemberDetails.this, Statistics.class);
-        intent.putExtra("image",R.drawable.deepakchahar11);
+        intent.putExtra("image", R.drawable.deepakchahar11);
         startActivity(intent);
     }
 
     public void onPrithvi(View view) {
         intent = new Intent(MemberDetails.this, Statistics.class);
-        intent.putExtra("image",R.drawable.prithvishaw12);
+        intent.putExtra("image", R.drawable.prithvishaw12);
         startActivity(intent);
     }
 
     public void onNitish(View view) {
         intent = new Intent(MemberDetails.this, Statistics.class);
-        intent.putExtra("image",R.drawable.nitishrana13);
+        intent.putExtra("image", R.drawable.nitishrana13);
         startActivity(intent);
     }
 
     public void onKlRahul(View view) {
         intent = new Intent(MemberDetails.this, Statistics.class);
-        intent.putExtra("image",R.drawable.klrahul14);
+        intent.putExtra("image", R.drawable.klrahul14);
         startActivity(intent);
     }
 
     public void onKuldeep(View view) {
         intent = new Intent(MemberDetails.this, Statistics.class);
-        intent.putExtra("image",R.drawable.kuldeepyadav15);
+        intent.putExtra("image", R.drawable.kuldeepyadav15);
         startActivity(intent);
     }
 
     public void onKrunal(View view) {
         intent = new Intent(MemberDetails.this, Statistics.class);
-        intent.putExtra("image",R.drawable.krunalpandya16);
+        intent.putExtra("image", R.drawable.krunalpandya16);
         startActivity(intent);
     }
 
     public void onShardul(View view) {
         intent = new Intent(MemberDetails.this, Statistics.class);
-        intent.putExtra("image",R.drawable.shardulnarendrthakur17);
+        intent.putExtra("image", R.drawable.shardulnarendrthakur17);
         startActivity(intent);
     }
 
     public void onCoachDravid(View view) {
         intent = new Intent(MemberDetails.this, Statistics.class);
-        intent.putExtra("image",R.drawable.coachrahuldravid);
+        intent.putExtra("image", R.drawable.coachrahuldravid);
         startActivity(intent);
     }
 
     public void onCoachSridhar(View view) {
         intent = new Intent(MemberDetails.this, Statistics.class);
-        intent.putExtra("image",R.drawable.coachshridhar);
+        intent.putExtra("image", R.drawable.coachshridhar);
         startActivity(intent);
     }
 
     public void onCoachBharat(View view) {
         intent = new Intent(MemberDetails.this, Statistics.class);
-        intent.putExtra("image",R.drawable.coachbharatarun);
+        intent.putExtra("image", R.drawable.coachbharatarun);
         startActivity(intent);
+    }
+
+    public static void commonDetails(String name,String role,String matchTestBat,String matchOdiBat,String matchT20Bat,
+                                     String matchTestBowl,String matchOdiBowl,String matchT20Bowl){
+        txtName=name;
+        txtRole=role;
+        txtMatchTestBat=matchTestBat;
+        txtMatchOdiBat=matchOdiBat;
+        txtMatchT20Bat=matchT20Bat;
+        txtMatchTestBowl=matchTestBowl;
+        txtMatchOdiBowl=matchOdiBowl;
+        txtMatchT20Bowl=matchT20Bowl;
+
+    }
+
+    public static void battingDetails(String runsTest,String fiftysTest,String hundredsTest,String runsOdi,String fiftysOdi,
+                                      String hundredsOdi,String runsT20,String fiftysT20,String hundredsT20){
+        txtRunsTest=runsTest;
+        txt50sTest=fiftysTest;
+        txt100sTest=hundredsTest;
+        txtRunsOdi=runsOdi;
+        txt50sOdi=fiftysOdi;
+        txt100sOdi=hundredsOdi;
+        txtRunsT20=runsT20;
+        txt50sT20=fiftysT20;
+        txt100sT20=hundredsT20;
+
+    }
+
+    public static void bowlingDetails(String wicketsTest,String ecoTest,String bestTest,String wicketsOdi,String ecoOdi,String bestOdi,String wicketsT20,String ecoT20,String bestT20){
+        txtWicketsTest=wicketsTest;
+        txtEcoTest=ecoTest;
+        txtBestTest=bestTest;
+        txtWicketsOdi=wicketsOdi;
+        txtEcoOdi=ecoOdi;
+        txtBestOdi=bestOdi;
+        txtWicketsT20=wicketsT20;
+        txtEcoT20=ecoT20;
+        txtBestT20=ecoT20;
     }
 
 
--- app/src/main/java/com/satyam/cricketstatus/Statistics.java
+++ app/src/main/java/com/satyam/cricketstatus/Statistics.java
@@ -6,18 +6,117 @@ import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Statistics extends AppCompatActivity {
+    String  txtName1, txtRole2, txtMatchTestBat3, txtMatchOdiBat4, txtMatchT20Bat5,
+            txtMatchTestBowl6, txtMatchOdiBowl7, txtMatchT20Bowl8,txtRunsTest9,
+            txt50sTest10, txt100sTest11, txtRunsOdi12, txt50sOdi13, txt100sOdi14,
+            txtRunsT2015, txt50sT2016, txt100sT2017,txtWicketsTest18, txtEcoTest19,
+            txtBestTest20, txtWicketsOdi21, txtEcoOdi22, txtBestOdi23, txtWicketsT2024, txtEcoT2025, txtBestT2026;;
+
+    TextView txtName, txtRole, txtMatchTestBat, txtMatchOdiBat, txtMatchT20Bat,
+            txtMatchTestBowl, txtMatchOdiBowl, txtMatchT20Bowl;
+    TextView txtRunsTest, txt50sTest, txt100sTest, txtRunsOdi, txt50sOdi, txt100sOdi, txtRunsT20, txt50sT20, txt100sT20;
+
+    TextView txtWicketsTest, txtEcoTest, txtBestTest, txtWicketsOdi, txtEcoOdi, txtBestOdi, txtWicketsT20, txtEcoT20, txtBestT20;
     ImageView playerImage;
     int imagevalue;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
-        playerImage=findViewById(R.id.playerImage);
+        playerImage = findViewById(R.id.playerImage);
+        txtName = findViewById(R.id.txtName);
+        txtRole = findViewById(R.id.txtRole);
+        txtMatchTestBat = findViewById(R.id.txtMatchTestBat);
+        txtMatchOdiBat = findViewById(R.id.txtMatchOdiBat);
+        txtMatchT20Bat = findViewById(R.id.txtMatchT20Bat);
+        txtMatchTestBowl = findViewById(R.id.txtMatchTestBowl);
+        txtMatchOdiBowl = findViewById(R.id.txtMatchOdiBowl);
+        txtMatchT20Bowl = findViewById(R.id.txtMatchT20Bowl);
+
+        txtRunsTest = findViewById(R.id.txtRunsTest);
+        txt50sTest = findViewById(R.id.txt50sTest);
+        txt100sTest = findViewById(R.id.txt100sTest);
+        txtRunsOdi = findViewById(R.id.txtRunsOdi);
+        txt50sOdi = findViewById(R.id.txt50sOdi);
+        txt100sOdi = findViewById(R.id.txt100sOdi);
+        txtRunsT20 = findViewById(R.id.txtRunsT20);
+        txt50sT20 = findViewById(R.id.txt50sT20);
+        txt100sT20 = findViewById(R.id.txt100sT20);
+
+        txtWicketsTest = findViewById(R.id.txtWicketsTest);
+        txtEcoTest = findViewById(R.id.txtEcoTest);
+        txtBestTest = findViewById(R.id.txtBestTest);
+        txtWicketsOdi = findViewById(R.id.txtWicketsOdi);
+        txtEcoOdi = findViewById(R.id.txtEcoOdi);
+        txtBestOdi = findViewById(R.id.txtBestOdi);
+        txtWicketsT20 = findViewById(R.id.txtWicketsT20);
+        txtEcoT20 = findViewById(R.id.txtEcoT20);
+        txtBestT20 = findViewById(R.id.txtBestT20);
+
         Bundle bundle = getIntent().getExtras();
         imagevalue = bundle.getInt("image");
         playerImage.setImageResource(imagevalue);
+
+        Intent intent=getIntent();
+        txtName1=intent.getStringExtra("txtName");
+        txtRole2=intent.getStringExtra("txtRole");
+        txtMatchTestBat3=intent.getStringExtra("txtMatchTestBat");
+        txtMatchOdiBat4=intent.getStringExtra("txtMatchOdiBat");
+        txtMatchT20Bat5=intent.getStringExtra("txtMatchT20Bat");
+        txtMatchTestBowl6=intent.getStringExtra("txtMatchTestBowl");
+        txtMatchOdiBowl7=intent.getStringExtra("txtMatchOdiBowl");
+        txtMatchT20Bowl8=intent.getStringExtra("txtMatchT20Bowl");
+        txtRunsTest9=intent.getStringExtra("txtRunsTest");
+        txt50sTest10=intent.getStringExtra("txt50sTest");
+        txt100sTest11=intent.getStringExtra("txt100sTest");
+        txtRunsOdi12=intent.getStringExtra("txtRunsOdi");
+        txt50sOdi13=intent.getStringExtra("txt50sOdi");
+        txt100sOdi14=intent.getStringExtra("txt100sOdi");
+        txtRunsT2015=intent.getStringExtra("txtRunsT20");
+        txt50sT2016=intent.getStringExtra("txt50sT20");
+        txt100sT2017=intent.getStringExtra("txt100sT20");
+        txtWicketsTest18=intent.getStringExtra("txtWicketsTest");
+        txtEcoTest19=intent.getStringExtra("txtEcoTest");
+        txtBestTest20=intent.getStringExtra("txtBestTest");
+        txtWicketsOdi21=intent.getStringExtra("txtWicketsOdi");
+        txtEcoOdi22=intent.getStringExtra("txtEcoOdi");
+        txtBestOdi23=intent.getStringExtra("txtBestOdi");
+        txtWicketsT2024=intent.getStringExtra("txtWicketsT20");
+        txtEcoT2025=intent.getStringExtra("txtEcoT20");
+        txtBestT2026=intent.getStringExtra("txtBestT20");
+
+        txtName.setText(txtName1);
+        txtRole.setText(txtRole2);
+        txtMatchTestBat.setText(txtMatchTestBat3);
+        txtMatchOdiBat.setText(txtMatchOdiBat4);
+        txtMatchT20Bat.setText(txtMatchT20Bat5);
+        txtMatchTestBowl.setText(txtMatchTestBowl6);
+        txtMatchOdiBowl.setText(txtMatchOdiBowl7);
+        txtMatchT20Bowl.setText(txtMatchT20Bowl8);
+
+        txtRunsTest.setText(txtRunsTest9);
+        txt50sTest.setText(txt50sTest10);
+        txt100sTest.setText(txt100sTest11);
+        txtRunsOdi.setText(txtRunsOdi12);
+        txt50sOdi.setText(txt50sOdi13);
+        txt100sOdi.setText(txt100sOdi14);
+        txtRunsT20.setText(txtRunsT2015);
+        txt50sT20.setText(txt50sT2016);
+        txt100sT20.setText(txt100sT2017);
+
+        txtWicketsTest.setText(txtWicketsTest18);
+        txtEcoTest.setText(txtEcoTest19);
+        txtBestTest.setText(txtBestTest20);
+        txtWicketsOdi.setText(txtWicketsOdi21);
+        txtEcoOdi.setText(txtEcoOdi22);
+        txtBestOdi.setText(txtBestOdi23);
+        txtWicketsT20.setText(txtWicketsT2024);
+        txtEcoT20.setText(txtEcoT2025);
+        txtBestT20.setText(txtBestT2026);
 
     }
 }
